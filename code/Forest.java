296413586997fa0fdf14114a2e4dfa3c5bc4133f
triciaglassborow <22212293@bucks.ac.uk>
2023-02-03 10:43:30 +0000

    possibility done - tricia

--- code/Forest.java
+++ code/Forest.java
@@ -1,1 +1,23 @@
 package code;
+
+public class Forest
+{
+    int input;
+
+    public static void main(String[] args)
+    {
+        getPossibility();
+
+    }
+
+    public static Possibility getPossibility()
+    {
+        // created temp variables
+        int tempInput;
+        //assigning values to the variables
+        tempInput = 50;
+        //returning input
+        return new Possibility(tempInput);
+    }
+}
+   
--- code/Possibility.java
+++ code/Possibility.java
@@ -1,5 +1,31 @@
 package code;
 
-public class Possibility {
+
+public class Possibility 
+{
+    int input;
+    int random_int;
+    boolean outcome;
+    //constructor
+    public Possibility(int input)
+    {
+        this.input = input;
+
+        int min = 1; // Minimum value of range
+        int max = 100; // Maximum value of range
+        // Generate random int value from min to max
+        int random_int = (int)Math.floor(Math.random() * (max - min + 1) + min);
+         // comparing random and input
+        if(random_int <= input)
+        {
+            outcome = true;
+        }
+        else
+        {
+            outcome = false;
+        }
     
+    }
+
+
 }
