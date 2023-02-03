be3f14ee5662f2f7829174686d9898e847aeb9e1
triciaglassborow <22212293@bucks.ac.uk>
2023-02-03 11:13:46 +0000

    Possibility Final + Testing - Tricia

--- code/Possibility.java
+++ code/Possibility.java
@@ -3,9 +3,11 @@ package code;
 
 public class Possibility 
 {
+    // creating variables 
     int input;
     int random_int;
     boolean outcome;
+
     //constructor
     public Possibility(int input)
     {
@@ -15,6 +17,7 @@ public class Possibility
         int max = 100; // Maximum value of range
         // Generate random int value from min to max
         int random_int = (int)Math.floor(Math.random() * (max - min + 1) + min);
+
          // comparing random and input
         if(random_int <= input)
         {
